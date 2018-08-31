261fc4db9f04c73dd813b4f9582a1d4387d80b75
award94 <adam.m.ward.94@gmail.com>
2018-08-31 13:15:09 -0500

    Did the splitlistbydpn function

--- COSC4315 HW1/inputhandler.py
+++ COSC4315 HW1/inputhandler.py
@@ -4,6 +4,9 @@ class inputhandler:
     def __init__(self, rawInput, dpn):
         self.rawInput = rawInput            #the math term we're equating
         self.dpn = dpn                      #digits per node
+        if(dpn == 0):
+            print("ERROR: digitsPerNode = 0")
+            return -1
         self.term1 = []                     #left hand term
         self.term2 = []                     #right hand term
         self.term3 = []                     #the result
@@ -44,7 +47,76 @@ class inputhandler:
 
 #   Splits the list into dpn sized nodes
     def splitlistbydpn(self):
-        print("inside splitlistbydpn()")
+        #print("inside splitlistbydpn()")
+        #print(self.term1.__len__()%self.dpn)
+        offset1 = self.term1.__len__()%self.dpn
+        offset2 = self.term2.__len__()%self.dpn
+        #print(offset1)
+        tempList1 = []
+        tempList2 = []
+        tempString = ""
+        #print(tempList1)
+        #print(tempList2)
+
+        #if head node is a different size than dpn
+        def addHead(tempList, oldList, tempString, i, offset):
+            print("i=", i)
+            if(i >= offset):
+                print("if")
+                tempList.append(tempString)
+                return -1
+            else:
+                print("else")
+                print("oldList[",i,"]=",oldList[i])
+                print("before=", tempString)
+                tempString = tempString + oldList[i]
+                print("after=", tempString)
+                addHead(tempList, oldList, tempString, i+1, offset)
+
+        #if head node is equal size to dpn
+        if(offset1 != 0):
+            addHead(tempList1, self.term1, tempString, 0, offset1)
+            print("tempList1[0]=", tempList1[0])
+
+        if(offset2 != 0):
+            addHead(tempList2, self.term2, tempString, 0, offset2)
+            print("tempList2[0]=", tempList2[0])
+
+        #number of nodes - headnode
+        numNodes1 = (self.term1.__len__() - offset1) / self.dpn
+        numNodes2 = (self.term2.__len__() - offset2) / self.dpn
+        print(numNodes1)
+        print(numNodes2)
+
+        #nested loop
+        #adds the individual chars to each node
+        def addCharsToNode(tempList, oldList, tempString, i, j, offset):
+            print('inside addCharsToNode')
+            if i >= self.dpn:
+                print("if")
+                tempList.append(tempString)
+                return -1
+            else:
+                print("else")
+                print("self.term[", i+offset + self.dpn*j, "]=", oldList[i + offset + self.dpn*j])
+                tempString = tempString + oldList[i + offset + self.dpn*j]
+                addCharsToNode(tempList, oldList, tempString, i+1, j, offset)
+
+        #adds the nodes to the list
+        def addNodesToList(tempList, oldList, j, numNodes, offset):
+            print('inside addNodesToList')
+            if j >= numNodes:
+                print("if")
+                return -1
+            else:
+                print("else")
+                addCharsToNode(tempList, oldList, tempString, 0, j, offset)
+                addNodesToList(tempList, oldList, j+1, numNodes, offset)
+
+        addNodesToList(tempList1, self.term1, 0, numNodes1, offset1)
+        addNodesToList(tempList2, self.term2, 0, numNodes2, offset2)
+        print(tempList1)
+        print(tempList2)
 
 #   Does the math operation
     def domath(self):
--- COSC4315 HW1/main.py
+++ COSC4315 HW1/main.py
@@ -1,25 +1,28 @@
 import inputhandler as ih
 
-digitsPerNode = 4
-input = "1234+2222"
 
+#Need to read these two variables inline, but can do that later
+digitsPerNode = 3
+input = "1234567+2222222"
+
+#main(){
 ih = ih.inputhandler(input, digitsPerNode)
 ih.parseraw()
 ih.splitlistbydpn()
 ih.domath()
 
+#}
+
 #Just messing around with .split()
 #I don't see a feasable way to split
 #the strings into correct size nodes
 #without using iteration.
-print(input)
-print([input[i:i+2] for i in range(0, len(input), 2)])
-print(input.split())
-newLists = input.split('+')
-print(newLists)
-newString1 = "".join(newLists[0])
-newString2 = "".join(newLists[1])
-print(newString1)
-print(newString2)
-
-print(ih.term1.__len__())
+#print(input)
+#print([input[i:i+2] for i in range(0, len(input), 2)])
+#print(input.split())
+#newLists = input.split('+')
+#print(newLists)
+#newString1 = "".join(newLists[0])
+#newString2 = "".join(newLists[1])
+#print(newString1)
+#print(newString2)
