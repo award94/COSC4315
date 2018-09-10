514ad469d9f02f0ecd09427b99c0945b218326e8
award94 <adam.m.ward.94@gmail.com>
2018-09-10 18:25:31 -0500

    Added multiplication

--- COSC4315 HW1/inputhandler.py
+++ COSC4315 HW1/inputhandler.py
@@ -17,7 +17,7 @@ class inputhandler:
 
 #   will change input string into managable data
     def parseraw(self):
-        print("inside parseraw()")
+        #print("inside parseraw()")
 
 #       for iterating through rawInput
         parsingIndex = 0
@@ -44,9 +44,9 @@ class inputhandler:
         recurparse(self.rawInput, self.term1, 0, self.op)
 
 #       prints to see what's happening. Can delete
-        print(self.term1)
-        print(self.op)
-        print(self.term2)
+        #print(self.term1)
+        #print(self.op)
+        #print(self.term2)
 
 #   Splits the list into dpn sized nodes
     def splitlistbydpn(self):
@@ -63,63 +63,61 @@ class inputhandler:
 
         #if head node is a different size than dpn
         def addHead(tempList, oldList, tempString, i, offset):
-            print("i=", i)
+            #print("i=", i)
             if(i >= offset):
-                print("if")
+                #print("if")
                 tempList.append(tempString)
                 return -1
             else:
-                print("else")
-                print("oldList[",i,"]=",oldList[i])
-                print("before=", tempString)
+                #print("else")
+                #print("oldList[",i,"]=",oldList[i])
+                #print("before=", tempString)
                 tempString = tempString + oldList[i]
-                print("after=", tempString)
+                #print("after=", tempString)
                 addHead(tempList, oldList, tempString, i+1, offset)
 
         #if head node is equal size to dpn
         if(offset1 != 0):
             addHead(tempList1, self.term1, tempString, 0, offset1)
-            print("tempList1[0]=", tempList1[0])
+            #print("tempList1[0]=", tempList1[0])
 
         if(offset2 != 0):
             addHead(tempList2, self.term2, tempString, 0, offset2)
-            print("tempList2[0]=", tempList2[0])
+            #print("tempList2[0]=", tempList2[0])
 
         #number of nodes - headnode
         numNodes1 = (self.term1.__len__() - offset1) / self.dpn
         numNodes2 = (self.term2.__len__() - offset2) / self.dpn
-        print(numNodes1)
-        print(numNodes2)
+        #print(numNodes1)
+        #print(numNodes2)
 
         #nested loop
         #adds the individual chars to each node
         def addCharsToNode(tempList, oldList, tempString, i, j, offset):
-            print('inside addCharsToNode')
+            #print('inside addCharsToNode')
             if i >= self.dpn:
-                print("if")
+                #print("if")
                 tempList.append(tempString)
                 return -1
             else:
-                print("else")
-                print("self.term[", i+offset + self.dpn*j, "]=", oldList[i + offset + self.dpn*j])
+                #print("else")
+                #print("self.term[", i+offset + self.dpn*j, "]=", oldList[i + offset + self.dpn*j])
                 tempString = tempString + oldList[i + offset + self.dpn*j]
                 addCharsToNode(tempList, oldList, tempString, i+1, j, offset)
 
         #adds the nodes to the list
         def addNodesToList(tempList, oldList, j, numNodes, offset):
-            print('inside addNodesToList')
+            #print('inside addNodesToList')
             if j >= numNodes:
-                print("if")
+                #print("if")
                 return -1
             else:
-                print("else")
+                #print("else")
                 addCharsToNode(tempList, oldList, tempString, 0, j, offset)
                 addNodesToList(tempList, oldList, j+1, numNodes, offset)
 
         addNodesToList(tempList1, self.term1, 0, numNodes1, offset1)
         addNodesToList(tempList2, self.term2, 0, numNodes2, offset2)
-        print(tempList1)
-        print(tempList2)
 
         self.listTerm1 = tempList1
         self.listTerm2 = tempList2
@@ -134,9 +132,72 @@ class inputhandler:
             print(term1.__len__())
             print(term2.__len__())
 
-        def multiplication(term1, term2):
+
+        def multiplication(term1, term2, term3):
             print("inside multiplication()")
             print(term1, '*', term2)
+            print(term1, '*', term2)
+            print("term1.len=",term1.__len__())
+            print("term2.len=",term2.__len__())
+            term3length = term1.__len__() + term2.__len__()
+            print("term3.len=", term3length)
+
+            def addEmptyNodes(term3, i):
+                if(i > 0):
+                    term3.append(0)
+                    addEmptyNodes(term3, i-1)
+
+            addEmptyNodes(term3, term3length)
+            modShift = 10**self.dpn
+            print(modShift)
+            print(term3)
+            print("t3len=", term3length)
+
+            def multiply(term3, i, j, carryOver):
+                if(i >= 0):
+                    print("inside multiply, i=",i)
+                    multiplyNested(term3, i, j, carryOver)
+                    multiply(term3, i-1, j, carryOver)
+
+            def multiplyNested(term3, i, j, carryOver):
+                if j >= 0:
+                    #Help for debugging
+                    print("\n\n-------------------------------------------")
+                    print("inside multiplyNested, j=", j)
+                    print(term3)
+                    print("\t term1[", i, "]=", term1[i], "term2[", j, "]=", term2[j])
+                    print("\t carryOver=", carryOver, "\n")
+
+                    #Finding the right index to add product to
+                    t1Index = term1.__len__() - i - 1
+                    t2Index = term2.__len__() - j - 1
+                    print("t1Index=",t1Index)
+                    print("t2Index=",t2Index)
+                    k = term3length - t1Index - t2Index - 1
+                    print("k=",k)
+
+                    #tricky math stuff
+                    product = int(term1[i]) * int(term2[j])
+                    print("product=",product)
+                    #adding carry from current mult to k-1
+                    carryOver = int(product/modShift)
+                    term3[k-1] = term3[k-1] + carryOver
+                    print("carryOver=",carryOver)
+                    print("adding to ", int(product%modShift))
+                    #adding actual product to k
+                    term3[k] = term3[k] + int(product%modShift)
+                    #adding any overflow in k from addition to k-1 and trimming k to dpn size
+                    carryOver = int(term3[k] / modShift)
+                    term3[k] = int(term3[k]%modShift)
+                    term3[k-1] = term3[k-1] + carryOver
+                    #resulting k node
+                    print("resulting node=", term3[k])
+
+                    multiplyNested(term3, i, j-1, carryOver)
+
+            multiply(term3, term1.__len__()-1, term2.__len__()-1, 0)
+
+            print("\n------------------------\n------------------------\nResult of all math=",term3)
 
         print(self.op)
         if(self.op[0] == '+'):
@@ -144,7 +205,6 @@ class inputhandler:
             addition(self.listTerm1, self.listTerm2)
         elif(self.op[0] == '*'):
             print("Multiplication")
-            multiplication(self.listTerm1, self.listTerm2)
+            multiplication(self.listTerm1, self.listTerm2, self.listTerm3)
         else:
             print("Invalid Operator");
-
