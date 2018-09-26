bab9ae7cd6e41943280243e27cbf47e24993bc70
award94 <adam.m.ward.94@gmail.com>
2018-09-26 17:43:43 -0500

    added some basic stuff

---
+++ HW2/README.md
@@ -0,0 +1,3 @@
+usage:
+g++ -o hw2.exe hw2.cpp
+./hw2.exe < input.txt
---
+++ HW2/hw2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	cout<< "Homework 2"<<endl;
+	
+	string line;
+	while (getline(cin, line))
+		cout<<line<<endl;
+		
+	return 0;	
+}
