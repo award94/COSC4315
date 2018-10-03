c73ccf0e0063334a4420671536609e6fdf788f6b
hdy9464 <37273225+hdy9464@users.noreply.github.com>
2018-10-03 00:04:50 -0500

    Made a very basic parser, still need to add in checks for const int and looking up the variable. For some reason it cuts off the last part of the substring.

--- HW2/token_print.h
+++ HW2/token_print.h
@@ -1,10 +1,33 @@
 using namespace std;
 
 void print(string line, list<variable*> & Variables) {
-	// Send the whole lin efrom main function call. once in thhe function parse out the string inside print(). if thehre are const values
+	//string line = "something something, darkside, hello, its me";
+	string delimiter = ",";
+	string finalOutput;
+	auto start = 0U;
+	auto end = line.find(delimiter);
+	
+	
+
+	// Alternative method
+	while (end != std::string::npos) {
+		cout << line.substr(start, end - start) << endl;
+		start = end + delimiter.length();
+		end = line.find(delimiter, start);
+	}
+
+/*
+	// Create a string object that will be cout'd at the end
+	string finalOutput;
+	// Parsing the argument line that will serve as the input to the print statement
+	cout << "Final value of line is: " << line << endl;	
+
+
 	// append them to the string you're printing. if they're variales 
 	cout << "inside print()" << endl;
 	// Check for type of input, if type is string or anything besides int, just cout it
 	// If type is int call the arithmetic func Adam wrote
 	// Parse with a comma delimiter
+*/
 }
+
